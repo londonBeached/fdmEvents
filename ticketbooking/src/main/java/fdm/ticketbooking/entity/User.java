d2c83255f2723b7e7f54b25cb001b074a96dd475
jasdeep.aulakh <jasdeep.aulakh@fdmgroup.com>
2017-01-25 11:39:42 +0000

    Converted project to webapp project and added Spring MVC jars

---
+++ ticketbooking/src/main/java/fdm/ticketbooking/entity/User.java
@@ -0,0 +1,75 @@
+package fdm.ticketbooking.entity;
+
+import java.util.Date;
+
+public class User {
+	
+	public String email;
+	public String password;
+	public String firstName;
+	public String lastName;
+	public String gender;
+	public Date dob;
+	
+	public User(String email, String password, String firstName, String lastName, String gender, Date dob) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.dob = dob;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+	
+	
+	
+
+}
