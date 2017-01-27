e8c7dfdf4874e61c9a51c37b280c1cd3b5881df3
peter.dyer <peter.dyer@TRNLON11507.fdmgroup.local>
2017-01-27 16:42:14 +0000

    might have the data being captured

--- ticketbooking/src/main/java/fdm/ticketbooking/controller/LoginController.java
+++ ticketbooking/src/main/java/fdm/ticketbooking/controller/LoginController.java
@@ -2,16 +2,27 @@ package fdm.ticketbooking.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import fdm.ticketbooking.entity.LoginForm;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping("/login")
-	public void loginPage(Model model){
-		
+	@GetMapping("/login")
+	public String loginPage(Model model){
+		model.addAttribute("login", new LoginForm());
+		return "login";
 	}
 	
+	
+	
+	//Test that this  is actually receiving the data when you submit it!
+	@PostMapping("/login")
+	public String greetingSubmit(@ModelAttribute LoginForm login){
+		return "result";
+	}
 	
 	public boolean login(String username, String password){
 		return false;
